3a65877dfb07616187abae39b87adedd4d3109c8
Punk73 <teguhsetiawan171@gmail.com>
2025-06-11 10:52:09 +0700

    add select instead of inputing the url

--- gui.py
+++ gui.py
@@ -10,10 +10,20 @@ def open_config_gui():
     def on_save():
         global stream_url_result
         # Save the stream URL and close the window
-        stream_url_result = url_input.get() or default_url
+        # stream_url_result = url_input.get() or default_url
+        stream_url_result = url_input.get().strip() or url_presets[option_menu.get()]
         root.destroy()
 
     default_url = 'https://s3klari.qumicon.info:8888/camFix-F3/stream.m3u8'
+
+
+    # Named presets
+    url_presets = {
+        "Rel Munjul - Purwakarta": 'https://cctv.purwakartakab.go.id/cctv/rel-munjul.m3u8?v=4066696',
+        "Klari - Arah Cikampek": 'https://s3klari.qumicon.info:8888/camFix-F2/stream.m3u8',
+        "Klari - Arah Pintu Tol Karawang Timur": 'https://s3klari.qumicon.info:8888/camFix-F3/stream.m3u8',
+        "Klari - Arah Karawang": 'https://s3klari.qumicon.info:8888/camFix-F1/stream.m3u8',
+    }
 
     customtkinter.set_appearance_mode('dark')
     customtkinter.set_default_color_theme('dark-blue')
@@ -25,7 +35,14 @@ def open_config_gui():
     frame = customtkinter.CTkFrame(master=root)
     frame.pack(pady=20, padx=20, fill='both', expand=True)
 
-    label = customtkinter.CTkLabel(master=frame, text='Input Stream URL:')
+    label1 = customtkinter.CTkLabel(master=frame, text='Pilih Lokasi CCTV:')
+    label1.pack(pady=5)
+
+    option_menu = customtkinter.CTkOptionMenu(master=frame, values=list(url_presets.keys()))
+    option_menu.pack(pady=5)
+    option_menu.set("Klari - Arah Pintu Tol Karawang Timur")  # Default selection
+
+    label = customtkinter.CTkLabel(master=frame, text='Atau Input Stream URL:')
     label.pack(pady=5, padx=5)
 
     url_input = customtkinter.CTkEntry(master=frame, placeholder_text='e.g., https://...')
