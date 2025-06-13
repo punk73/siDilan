b04af1de221b237df4c3157b7904241e21e5bab0
punk73 <teguhsetiawan171@gmail.com>
2025-06-13 14:14:59 +0700

    ubah states supaya jalan ketika aplikasi sudah jadi exe

--- states.py
+++ states.py
@@ -1,9 +1,17 @@
 import json
 import os
+import sys
 
 def saveToText(line_points, file_path = "line_points.json"):
     with open(file_path, "w") as file:
         json.dump(line_points, file)
+        
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller .exe"""
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
 
 def load(file_path = "line_points.json"):
     res = []
@@ -11,8 +19,9 @@ def load(file_path = "line_points.json"):
     # Read line_points from a text file
 
     # Check if the file exists before trying to load it
-    if os.path.exists(file_path):
-        with open( file_path, "r") as file:
+    full_path = resource_path(file_path)
+    if os.path.exists(full_path):
+        with open(full_path, "r") as file:
             res = json.load(file)
-    
+
     return res
